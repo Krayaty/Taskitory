9c5fd8bc5fe3d77b559bad3c527b11faa0053d7c
Krayaty <fabian.schwickert@me.com>
2022-01-06 14:31:04 +0100

    - Einfügen eines Standard Maven Spring-Boot-Projekts

---
+++ src/main/java/de/krayadev/taskitory/TaskitoryApplication.java
@@ -0,0 +1,13 @@
+package de.krayadev.taskitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskitoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskitoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/krayadev/taskitory/TaskitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.krayadev.taskitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskitoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
